b640b637dee27f71d02d84116980ed2c4edc1417
Peter <rphumulock@gmail.com>
2017-11-14 01:46:18 -0500

    First Push

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './css/materialize.min.css';
+import './css/style.css';
+import './js/init.js';
+import './js/materialize.min.js';
+import ish1 from './assets/Ish7.jpg';
+import ish2 from './assets/Ish6.jpg';
+
+const image1 = document.getElementById('background1').setAttribute('src', ish2);
+const image2 = document.getElementById('background2').setAttribute('src', ish1);
