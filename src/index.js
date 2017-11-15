2a5ca3a30d17207468529b429f70c487511104e8
Peter <rphumulock@gmail.com>
2017-11-15 01:14:28 -0500

    Website

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import './css/materialize.min.css';
 import './css/style.css';
 import './js/init.js';
 import './js/materialize.min.js';
-import ish1 from './assets/Ish7.jpg';
-import ish2 from './assets/Ish6.jpg';
+import boxing_ring from './assets/boxing_ring.jpg';
+import boxer from './assets/boxer.jpg';
 
-const image1 = document.getElementById('background1').setAttribute('src', ish2);
-const image2 = document.getElementById('background2').setAttribute('src', ish1);
+const image1 = document.getElementById('background1').setAttribute('src', boxer);
+const image2 = document.getElementById('background2').setAttribute('src', boxing_ring);
