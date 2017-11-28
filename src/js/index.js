6168805e790641f97fbd08a6381d6b0142d81eee
Peter <rphumulock@gmail.com>
2017-11-28 03:36:09 -0500

    Opt

--- src/js/index.js
+++ src/js/index.js
@@ -2,20 +2,26 @@ import Vivus from 'vivus';
 import drawing from '../assets/cobrasnek.svg'
 
 (function () {
-  window.onload = () => {
+  /*window.onload = () => {
     let visited = localStorage.getItem("visited");
     if (visited) {
       window.location.assign("home.html");
     } else {
       localStorage.setItem("visited", "true");
-      window.location.assign("home.html");
-      /*new Vivus(drawing, { duration: 100 }, () => {
-        window.setTimeout(() => {
-          window.location.assign("home.html");
-        }, 5000);
-      });*/
-    }
-  };
+      window.location.assign("home.html");*/
+  new Vivus('hi-there',
+    {
+      type: 'delayed',
+      duration: 200,
+      animTimingFunction: Vivus.EASE,
+      duration: 800
+    }, () => {
+      //  window.setTimeout(() => {
+      //  window.location.assign("home.html");
+      //}, 5000);
+    });
+  //}
+  //};
 })();
 
 
